5d9e6d64330a7d2af5b375c6bb00f70703942a6f
nagarro-samyak <incsppk9@amway.com>
2023-03-21 00:47:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <div>Name: tic-tac-toe</div>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
+
+export default App;
